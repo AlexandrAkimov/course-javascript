c3acaec7b0bcf7e78fd8a0d1892a3770fd2d5282
AlexandrAkimov <akimovy14072018@yandex.ru>
2023-03-29 17:18:16 +0300

    vp-w-2

--- projects/loft-photo-1/model.js
+++ projects/loft-photo-1/model.js
@@ -4,6 +4,22 @@ import photosDB from './photos.json';
 import friendsDB from './friends.json';
 
 export default {
-  getRandomElement(array) {},
-  getNextPhoto() {},
+  getRandomElement(array) {
+    if (!array.length) {
+      return null;
+    }
+
+    const index = Math.round(Math.random() * (array.length - 1));
+
+    return array[index];
+  },
+
+  async getNextPhoto() {
+    const friend = this.getRandomElement(this.friends.items);
+    const photos = await this.getFriendPhotos(friend.id);
+    const photo = this.getRandomElement(photos.items);
+    const size = this.findSize(photo);
+
+    return { friend, id: photo.id, url: size.url };
+  },
 };
